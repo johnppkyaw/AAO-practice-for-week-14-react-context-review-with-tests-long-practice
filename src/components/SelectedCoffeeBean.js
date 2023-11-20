19f7a48177ccd9fd6a2c3a56ab34a668e6a1e114
John <jjpyae@gmail.com>
2023-11-20 11:45:36 -0800

    Completed phase 1 and 2

--- src/components/SelectedCoffeeBean.js
+++ src/components/SelectedCoffeeBean.js
@@ -1,7 +1,13 @@
+// import { UseCoffee } from '../context/CoffeeContext';
+import { useContext } from 'react';
+import { CoffeeContext } from '../context/CoffeeContext';
+
 const SelectedCoffeeBean = () => {
+  const { coffeeBean } = useContext(CoffeeContext);
+  // const { coffeeBean } = UseCoffee();
   return (
     <div className="selected-coffee">
-      <h2>Current Selection: </h2>
+      <h2>Current Selection: {coffeeBean.name} </h2>
     </div>
   );
 }
